d9b90e5582e448d7a4498e0af733f51c86f738dd
Pablo Mattalia <pablomattalia@gmail.com>
2022-06-08 10:01:14 +0200

    Contador funcionando

---
+++ contador-de-clics/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import React, { useState } from "react";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+import Boton from "./componentes/Boton";
+import Contador from "./componentes/Contador";
+
+function App() {
+  const [numClics, setNumClic] = useState(0);
+  const manejarClic = () => {
+    setNumClic(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClic(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freeCodeCampLogo}
+          alt="Logo de freecodeCamp"
+        />
+      </div>
+      <h1>Contador de clicks Pablo Mattalia</h1>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Click" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
